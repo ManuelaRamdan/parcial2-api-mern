52e4d95bef04825165ef532d4394777b5f41354e
Jonathan <jonathan.p.garrido88@gmail.com>
2025-10-07 10:39:35 -0300

    getMateriaByIDProfe Revisar

--- src/routes/materiaRoutes.js
+++ src/routes/materiaRoutes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const materiaController = require("./../controllers/materiaController");
+const {authenticateToken} = require("./../middlewares/authMiddelware");
+const {isProfe} = require("./../middlewares/verificarRolMiddelware");
 
 // CRUD
+router.get("/:id", [authenticateToken, isProfe], materiaController.getMateriaByIdProfe);
+
 router.get("/", materiaController.getAllMaterias);
-router.get("/:id", materiaController.getMateriaById);
+//router.get("/:id", materiaController.getMateriaById); POST?
 router.post("/", materiaController.createMateria);
 router.put("/:id", materiaController.updateMateria);
 router.delete("/:id", materiaController.deleteMateria);
