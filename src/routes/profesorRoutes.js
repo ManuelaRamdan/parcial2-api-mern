b35e7342d69f1eaae878c009f915a50d695af0ee
Manuela <mramdan@gmail.com>
2025-11-28 15:56:54 -0300

    puse una ruta primero para que ñfuncione

--- src/routes/profesorRoutes.js
+++ src/routes/profesorRoutes.js
@@ -6,9 +6,9 @@ const {authenticateToken} = require("./../middlewares/authMiddelware");
 const {isAdmin, isProfe} = require("./../middlewares/verificarRolMiddelware");
 
 
+router.put("/alumno/dni/:dni", [authenticateToken, isProfe], profesorController.actualizarNotasAsistenciasDelAlumno);
 router.get("/me", [authenticateToken, isProfe], profesorController.profeGetMiInfo);
 router.get("/:id", [authenticateToken, isAdmin],profesorController.getProfesorById);
-router.put("/alumno/dni/:dni", [authenticateToken, isProfe], profesorController.actualizarNotasAsistenciasDelAlumno);
 router.get("/",[authenticateToken, isAdmin], profesorController.getAllProfesores);
 
 module.exports = router;
